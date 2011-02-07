4fbb2feb45e22e45e18fceea3a6fe3acd61c40f2
Sascha Wessel <swessel@gr4yweb.de>
2011-02-07 18:38:51 +0100

    refactored javascript

--- js/main.js
+++ js/main.js
@@ -1,49 +1,40 @@
 $(function () {
 	
-	var projects = $('#projects')
+	function effectFadeIn(element) {
+		$(element).fadeTo(2000, 1).fadeTo(2000, 0.5, effectFadeOut(element))
+	}
+	function effectFadeOut(element) {
+		$(element).fadeTo(2000, 0.5).fadeTo(2000, 1, effectFadeIn(element))
+	}
 	
-	$.getJSON('http://github.com/api/v2/json/repos/show/gr4y?callback=?', function(data, status) {
-		$.each(data.repositories, function() {
-			
-			var projectName = this.name;
-			var projectDescription = this.description;
-			var projectLanguage = this.language;
-			var projectUrl = this.url;
-			var isForked = this.fork;
+	var projects = $('#projects');
+	projects.ready(function() {
+		$.getJSON('http://github.com/api/v2/json/repos/show/gr4y?callback=?', function(data, status) {
+			$.each(data.repositories, function() {
+				var projectLink = document.createElement('a');
+				$(projectLink).attr('href', this.url);
+				$(projectLink).html(this.name);
+				
+				var title = document.createElement('h2');
+				$(title).append(projectLink).attr('class','project-title')
 
-			var title = document.createElement('h2');
-			var language = document.createElement('span');
-			$(language).html(projectLanguage);
-			var text = document.createElement('p');
-			var line = document.createElement ('li');	
+				var language = document.createElement('span');
+				$(language).html(this.language);
 				
-			$(line).hide();
-				
-			var projectLink = document.createElement('a');
-			$(projectLink).attr('href', projectUrl);
-			$(projectLink).html(projectName);
-			
-			$(title).append(projectLink).attr('class','project-title')
-			$(line).append(title).append(language);
-			
-			if(isForked) {
 				var forkBatch = document.createElement('span');
 				$(forkBatch).attr('class', 'forked').text('forked');
-				$(line).append(forkBatch);			
-			} else {
-				$(text).html(projectDescription);
-			}
-			$(line).append(text).attr('class', 'project');
-			
-			if(projectName != "gr4y.github.com") {
-				$(projects).append(line);				
-			}
-			
-			$(line).fadeIn(2000);
-			
+
+				var text = document.createElement('p');
+				var line = document.createElement ('li');					
+				$(line).hide().append(title).append(language).append(text).attr('class', 'project');
+				this.fork ? $(line).append(forkBatch) : $(text).html(this.description);
+				
+				if(this.name != "gr4y.github.com") {
+					$(projects).append(line);				
+				}
+				$(line).fadeIn(2000);
+			});
 		});
-	});
-	
-	$('#content').append(projects);
-	
+	})
+	// effectFadeOut('.avatar');
 });
