d52927c7e84ba94a7d9902091848642e2695610a
Sascha Wessel <swessel@gr4yweb.de>
2012-05-19 07:26:51 +0200

    and here it is. the big mustache. cleared up the main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
 $(function () {
-	
-	var projects = $('#projects');
-	projects.ready(function() {
-		$.getJSON('http://github.com/api/v2/json/repos/show/gr4y?callback=?', function(data, status) {
-			$.each(data.repositories, function() {
-				var projectLink = document.createElement('a');
-				$(projectLink).attr('href', this.url);
-				$(projectLink).html(this.name);
-				
-				var title = document.createElement('h2');
-				$(title).append(projectLink).attr('class','project-title')
-
-				var language = document.createElement('span');
-				$(language).html(this.language);
-				
-				var forkBatch = document.createElement('span');
-				$(forkBatch).attr('class', 'forked').text('forked');
-
-				var text = document.createElement('p');
-				var line = document.createElement ('li');					
-				$(line).hide().append(title).append(language).append(text).attr('class', 'project');
-				this.fork ? $(line).append(forkBatch) : $(text).html(this.description);
-				
-				if(this.name != "gr4y.github.com") {
-					$(projects).append(line);				
-				}
-				$(line).fadeIn(2000);
-			});
+	var projects = $("#projects");
+	$.getJSON('http://github.com/api/v2/json/repos/show/gr4y?callback=?', function(data, status) {
+		$.each(data.repositories, function() {
+			if(!this.fork && this.name != "gr4y.github.com") {
+				var projectTmpl = $('#projectTmpl').html();
+				$(projects).append(Mustache.to_html(projectTmpl, this));				
+			}
 		});
-	})
+	});
 });
