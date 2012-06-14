47b7bde8aafe23c274e3e63c9338609c2673d7d8
Sascha Wessel <swessel@gr4yweb.de>
2012-06-14 18:36:31 +0200

    forgot debug code. removed it nao

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 $(function () {
 	var projects = $("#projects");
 	$.getJSON('https://api.github.com/users/gr4y/repos', function(data, status) {
-		
-		console.log(data);
-		
 		$.each(data, function() {
 			if(!this.fork && this.name != "gr4y.github.com") {
 				var projectTmpl = $('#projectTmpl').html();
