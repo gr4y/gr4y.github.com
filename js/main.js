0d287f23e45e00ffd0be498c1bcec29ee740697f
Sascha Wessel <swessel@gr4yweb.de>
2012-06-14 18:40:11 +0200

    now i put back the jsonp callback. stupid security shite. ;-)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 	var projects = $("#projects");
-	$.getJSON('https://api.github.com/users/gr4y/repos', function(data, status) {
+	$.getJSON('https://api.github.com/users/gr4y/repos?callback=?', function(data, status) {
 		$.each(data, function() {
 			if(!this.fork && this.name != "gr4y.github.com") {
 				var projectTmpl = $('#projectTmpl').html();
